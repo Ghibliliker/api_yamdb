aff5dbe862ef02b4c50ab3dffde4ba068ac90533
Алексей Файферт <cetop8@yandex.ru>
2021-09-06 22:43:51 +0300

    v0.5

--- api_yamdb/comments/permissions.py
+++ api_yamdb/comments/permissions.py
@@ -10,5 +10,5 @@ class ReviewCommentPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
                 or obj.author == request.user
-                or request.user.role == 'moderator'
-                or request.user.role == 'admin')
+                or request.user.role == 'MO'
+                or request.user.role == 'AD')
