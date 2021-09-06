0bc26aa58b9a1e22bcee8a7bc95be49f07da5c28
Олег Силкин <dervish00.ovs@yandex.ru>
2021-09-06 22:18:03 +0300

    202109062217

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+from serializers import YamdbTokenObtainPairSerializer
+
+
+class YamdbTokenObtainPairView(TokenObtainPairView):
+    serializer_class = YamdbTokenObtainPairSerializer
