40a07f63f80b19d5f469587ba8642d00a7f7c718
Олег Силкин <dervish00.ovs@yandex.ru>
2021-09-06 20:50:49 +0300

    202109062050

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    USER = "US"
+    MODERATOR = "MO"
+    ADMIN = "AD"
+    ROLE_OF_USER_CHOICES = [
+        (USER, 'user'),
+        (MODERATOR, 'moderator'),
+        (ADMIN, 'admin'),
+    ]
+
+    email = models.EmailField(
+        max_length=254,
+        verbose_name="Электропочта",
+        blank=False,
+    )
+
+    bio = models.TextField(
+        verbose_name="Биография",
+        blank=True,
+    )
+
+    role = models.CharField(
+        max_length=2,
+        choices=ROLE_OF_USER_CHOICES,
+        default=USER,
+    )
