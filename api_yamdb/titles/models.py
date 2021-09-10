c3f3e8edefb359a0e7c2d1890b3628fa511370d8
Павел <pavel.maslo2015@yandex.com>
2021-09-10 10:50:27 +0300

    ver.0.9.1

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -1,10 +1,16 @@
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator, RegexValidator
 from django.db import models
 import datetime
 
+SLUG_REGEX = RegexValidator(r'^[-a-zA-Z0-9_]+$', 'неподходящий "slug"')
+
 class Category(models.Model):
     name = models.CharField(max_length=256)
-    slug = models.SlugField(unique=True, max_length=50)
+    slug = models.SlugField(
+        validators=[SLUG_REGEX],
+        unique=True, 
+        max_length=50
+    )  # ^[-a-zA-Z0-9_]+$  тест адекватности
 
     def __str__(self):
         return self.name
@@ -23,7 +29,7 @@ class Title(models.Model):
     year = models.IntegerField(
         'year',
         validators=[MinValueValidator(-10000), MaxValueValidator(datetime.date.today().year)]
-    )
+    )           # реализовать в сериализаторе?
     description = models.TextField(null=True, blank=True)
     genre = models.ManyToManyField(Genre, through='GenreTitle')
     category = models.ForeignKey(
