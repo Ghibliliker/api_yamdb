84ed262a876b8acc3c216b5bbda4074e850648e9
Ghibliliker <pavel.maslo2015@yandex.com>
2021-09-14 12:40:50 +0300

    ver1.2_user_title_test_ok

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -5,11 +5,14 @@ class AdminOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated)
+                or (request.user.is_authenticated
+                and (request.user.is_superuser or request.user.is_admin)))
 
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
-                or request.user.role == 'AD')
+                or (request.user.is_authenticated
+                and (request.user.is_superuser or request.user.is_admin)))
+
 
 
 class ReviewCommentPermission(permissions.BasePermission):
@@ -21,5 +24,5 @@ class ReviewCommentPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
                 or obj.author == request.user
-                or request.user.role == 'MO'
-                or request.user.role == 'AD')
+                or request.user.is_moderator
+                or request.user.is_admin)
